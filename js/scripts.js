6b089be1c1f12433b4884ae1193110df5024540c
Olabee87 <53159103+Olabee87@users.noreply.github.com>
2020-01-13 13:24:14 +0200

    First javascript test

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,5 @@
 console.log('That was it');
 
 
-var a = 1;
-var b = 2;
-var c = a + b;
-
-if (a < b) { 
-    // do the following things if a is lesser than b
-    console.log('a is lesser than b');
-    
-} else {
-    // do the following things otherwise
-    console.log('a greater than b');
-}
-
-if (a === 1) {
-    console.log('a is exactly 1');
-}
 
 
