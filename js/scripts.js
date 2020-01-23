a7b1b6f521969c9cb29d4dea5598a681e4297095
Olabee87 <53159103+Olabee87@users.noreply.github.com>
2020-01-23 14:45:37 +0200

    form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,85 +1,127 @@
 var registeredUsers = []; // this array stores valid usernames until the next pageload
 
-function validateForm(e){
-    e.preventDefault(); // stop the submit button from refreshing the page
-    console.log('validating....');
+if (registeredUsers.length > 5) {
+  registeredUsers.shift();
+}
 
-    console.log('user name: ' + validateUsername());
-    console.log('email: ' + validateEmail());
-    console.log('password: ' + validatePassword());
+function validateForm(e) {
+  e.preventDefault(); // stop the submit button from refreshing the page
+  console.log("validating....");
 
-    if (validateUsername() && validateEmail() && validatePassword()) {
-        var _newUser = getUserName();   
-        // add code to update registeredUsers array with new user and call render function
-        // TODO
-        document.registration.reset(); // reset form input fields
-    }
+  console.log("user name: " + validateUsername());
+  console.log("email: " + validateEmail());
+  console.log("password: " + validatePassword());
+  console.log("first_name: " + validateFirstName());
+  console.log("last_name: " + validateLastName());
+  console.log("phone_number: " + validatePhoneNumber());
+
+  if (validateUsername() && validateEmail() && validatePassword()) {
+    var _newUser = getUserName();
+    registeredUsers.push(_newUser);
+    renderRegisteredUsers();
+
+    // add code to update registeredUsers array with new user and call render function
+    // TODO
+    document.registration.reset(); // reset form input fields
+  }
 }
 
 function renderRegisteredUsers() {
-    registeredUsers.forEach(function(registeredUser){
-        var _newUser = document.createElement('li'); 
-        _newUser.innerHTML = registeredUser;
-        document.getElementById('registered-users').appendChild(_newUser);
-    });
+  registeredUsers.forEach(function(registeredUser) {
+    var _newUser = document.createElement("li");
+    _newUser.innerHTML = registeredUser;
+    document.getElementById("registered-users").appendChild(_newUser);
+  });
 }
 
 /**
  * this function supposely validates submitted username
  * @returns [Boolean] true when valid, false otherwise
  */
-function validateUsername(){
-    var _userName = getUserName();
-    
-    return !checkSpace(_userName);
+function validateUsername() {
+  var _userName = getUserName();
+
+  return !checkSpace(_userName);
 }
 
+function validateFirstName() {
+  var first_name = getFirstName();
+  if (first_name.length === " ") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateLastName() {
+  var first_name = getLastName();
+  if (last_name.length === " ") {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validatePhoneNumber() {
+  var phone_number = getPhoneNumber();
+  if (phone_number.length === "") {
+    return false;
+  } else {
+    return true;
+  }
+}
 /**
  * this function supposely validates submitted email
  * @returns [Boolean] true when valid, false otherwise
  */
-function validateEmail(){
-    var _email = getEmail();
+function validateEmail() {
+  var _email = getEmail();
 
-    if (checkSpace(_email) === true) {
-        return false;
-    }
+  if (checkSpace(_email) === true) {
+    return false;
+  }
 
-    // check for @
-    var atSymbol = _email.indexOf('@');
-    if(atSymbol < 1) {
-        return false;
-    }
+  // check for @
+  var atSymbol = _email.indexOf("@");
+  if (atSymbol < 1) {
+    return false;
+  }
 
-    // check if there is a dot located less than 2 symbols away from the @ sign
-    var dot = _email.indexOf('.');
-    if(dot <= atSymbol + 2) {
-        return false;
-    } 
+  // check if there is a dot located less than 2 symbols away from the @ sign
+  var dot = _email.indexOf(".");
+  if (dot <= atSymbol + 2) {
+    return false;
+  }
 
-    // check that the dot is not at the end
-    if (dot === _email.length - 1){
-        return false;
-    } 
+  // check that the dot is not at the end
+  if (dot === _email.length - 1) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
 /**
  * this function supposely validates submitted password
- * if password and confirmPassword do not match, return false 
- * 
+ * if password and confirmPassword do not match, return false
+ *
  * @returns [Boolean] true when valid, false otherwise
  */
 function validatePassword() {
-    var _password = getPassword();
-    var _confirmPassword = getConfirmPassword();
+  var _password = getPassword();
+  var _confirmPassword = getConfirmPassword();
 
-    if (_password !== _confirmPassword) {
-        return false;
-    }
+  if (_password.length < 8) {
+    alert("Are you kidding me? That thing is too short!");
+    return false;
+  }
 
-    return true;
+  if (_password !== _confirmPassword) {
+    alert("Both passwords do not match, check again");
+    return false;
+  }
+
+  return true;
 }
 
 /**
@@ -89,32 +131,73 @@ function validatePassword() {
  * @returns [Boolean] true when valid, false otherwise
  */
 function checkSpace(sample) {
-    return sample === '' || sample.indexOf(' ') > -1;
+  return sample === "" || sample.indexOf(" ") > -1;
 }
 
 /**
  * this function looks under the form with name "registration"
  * look under the "username" input field and returns the value of it
  * returns nothing if no value is found
- * 
+ *
  * @returns [Boolean] true when valid, false otherwise
  */
 function getUserName() {
-    if (typeof(document.registration.username.value) === 'undefined') {
-        return '';
-    } else {
-        return document.registration.username.value;
-    }   
+  if (typeof document.registration.username.value === "undefined") {
+    return "";
+  } else {
+    return document.registration.username.value;
+  }
+}
+
+function getFirstName() {
+  if (typeof document.registration.first_name.value === "undefined") {
+    return "";
+  } else {
+    return document.registration.first_name.value;
+  }
+}
+
+function getLastName() {
+  if (typeof document.registration.last_name.value === "undefined") {
+    return "";
+  } else {
+    return document.registration.last_name.value;
+  }
+}
+
+function getPhoneNumber() {
+  if (typeof document.registration.phone_number.value === "undefined") {
+    return "";
+  } else {
+    return document.registration.phone_number.value;
+  }
 }
 
 function getEmail() {
-    // TODO
+  if (typeof document.registration.email.value === "undefined") {
+    return "";
+  } else {
+    return document.registration.email.value;
+  }
 }
+
+// TODO
 
 function getPassword() {
-    // TODO
+  if (typeof document.registration.password.value === "undefined") {
+    return "";
+  } else {
+    return document.registration.password.value;
+  }
 }
 
+// TODO
+
 function getConfirmPassword() {
-    // TODO
+  if (typeof document.registration.password_confirm.value === "undefined") {
+    return "";
+  } else {
+    return document.registration.password_confirm.value;
+  }
 }
+// TODO
