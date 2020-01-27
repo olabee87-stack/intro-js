221f4106e64e9a627d95d8f2e5cb58cf27bc8f58
Olabee87 <53159103+Olabee87@users.noreply.github.com>
2020-01-27 13:50:49 +0200

    Prev_button

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,44 @@
-(function () {
-    var itemClassName = 'slider__photo';
-        items = document.getElementsByClassName(itemClassName),
-        totalItems = items.length,
-        slide = 0;
+(function() {
+  var itemClassName = "slider__photo";
+  (items = document.getElementsByClassName(itemClassName)),
+    (totalItems = items.length),
+    (slide = 0);
 
-    // Set event listeners
-    function setEventListeners() {
-        var next = document.getElementsByClassName('slider__button--next')[0],
-        var prev = document.getElementsByClassName('slider__button--prev')[0];
+  // Set event listeners
+  function setEventListeners() {
+    var next = document.getElementsByClassName("slider__button--next")[0],
+      prev = document.getElementsByClassName("slider__button--prev")[0];
 
-        next.addEventListener('click', getNext);
-        prev.addEventListener('click', getPrev);
-    }
+    next.addEventListener("click", getNext);
+    prev.addEventListener("click", getPrev);
+  }
 
-    function getNext() {
-          // TODO  
-        /*
-        get items with index of slide, remove active class from it
-        slide++ to increase the value of slide by 1
-        get items element with index of slide, add active class to it
+  function getNext() {
+    // TODO
+    /   //get items with index of slide, remove active class from it
+        //slide++ to increase the value of slide by 1
+        //get items element with index of slide, add active class to it
 
-        */
-        items[slide].classList.remove('active');
-        slide++;
-        items[slide].classList.add('active');
-        }
         
-       function getPrev(){
-        
-          
-    }
+    items[slide].classList.remove("active"); //This is the same as (items = document.getElementsByClassName(itemClassName)),
+    slide++; // This is increasing the slide by 1 on each click
+    items[slide].classList.add("active");
+  }
 
-    function initSlider() {
-        // setInitialClasses();
-        setEventListeners();
-        
-        // Set moving to false so that the slider becomes interactive
-        moving = false;
-    }
+  function getPrev() {
+      items[slide].classList.add('active');
+      slide--;
+      items[slide].classList.remove('active');
+  }
 
-    initSlider();
+  
+  function initSlider() {
+    // setInitialClasses();
+    setEventListeners();
+
+    // Set moving to false so that the slider becomes interactive
+    moving = false;
+  }
+
+  initSlider();
 })();
