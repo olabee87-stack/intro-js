6ccba51886988558fd10cc5b4afe6469f8341ec2
Olabee87 <53159103+Olabee87@users.noreply.github.com>
2020-01-27 13:42:48 +0200

    Slides review

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,28 @@
     // Set event listeners
     function setEventListeners() {
         var next = document.getElementsByClassName('slider__button--next')[0],
-            prev = document.getElementsByClassName('slider__button--prev')[0];
+        var prev = document.getElementsByClassName('slider__button--prev')[0];
 
         next.addEventListener('click', getNext);
         prev.addEventListener('click', getPrev);
     }
 
     function getNext() {
-        // TODO
-    }
+          // TODO  
+        /*
+        get items with index of slide, remove active class from it
+        slide++ to increase the value of slide by 1
+        get items element with index of slide, add active class to it
 
-    function getPrev(){
-        // TODO      
+        */
+        items[slide].classList.remove('active');
+        slide++;
+        items[slide].classList.add('active');
+        }
+        
+       function getPrev(){
+        
+          
     }
 
     function initSlider() {
