d654e02093f6b3ba4e59e066ac86ea0a0481bac1
Olabee87 <53159103+Olabee87@users.noreply.github.com>
2020-01-30 14:08:37 +0200

    with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -6,44 +6,44 @@
 
   // Set event listeners
   function setEventListeners() {
-    var next = document.getElementsByClassName("slider__button--next")[0],
-      prev = document.getElementsByClassName("slider__button--prev")[0];
+    var next = $(".slider_button--next")[0];
+    //var next = document.getElementsByClassName("slider__button--next")[0],
+    //prev = document.getElementsByClassName("slider__button--prev")[0];
+    prev = $(".slider_button--prev")[0];
 
     next.addEventListener("click", getNext);
-    prev.addEventListener("click", getPrev);
+    //prev.addEventListener("click", getPrev);
+    $(prev).on("click", getPrev); //jquery version
   }
 
-  
-    // TODO
-      //get items with index of slide, remove active class from it
-        //slide++ to increase the value of slide by 1
-        //get items element with index of slide, add active class to it
+  // TODO
+  //get items with index of slide, remove active class from it
+  //slide++ to increase the value of slide by 1
+  //get items element with index of slide, add active class to it
 
-    function getNext() {  
-    items[slide].classList.remove("active"); //This is the same as (items = document.getElementsByClassName(itemClassName)),
-    
-    if (slide === totalItems - 1){
-    slide = 0;
-    } else{
-    slide++;
+  function getNext() {
+    items[slide].removeClass("active"); //This is the same as (items = document.getElementsByClassName(itemClassName)),
+
+    if (slide === totalItems - 1) {
+      slide = 0;
+    } else {
+      slide++;
     } // This is increasing the slide by 1 on each click
-    items[slide].classList.add("active");
-}
-  
+    items[slide].addClass("active");
+  }
 
   function getPrev() {
-      items[slide].classList.remove('active');
+    items[slide].removeClass("active");
 
-      if (slide === 0){
-          slide = totalItems - 1;
-      } else {
-        slide--;
-      }
-     
-      items[slide].classList.add('active');
+    if (slide === 0) {
+      slide = totalItems - 1;
+    } else {
+      slide--;
+    }
+
+    items[slide].addClass("active");
   }
 
-  
   function initSlider() {
     // setInitialClasses();
     setEventListeners();
