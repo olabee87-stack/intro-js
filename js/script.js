bc0374c82d69061f1a755bb3ac5e926df451432b
Olabee87 <53159103+Olabee87@users.noreply.github.com>
2020-02-13 14:51:52 +0200

    clock done, to do music

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+function workingClock() {
+  var currentDate = new Date();
+  var hr = currentDate.getHours(),
+    min = currentDate.getMinutes(),
+    sec = currentDate.getSeconds(),
+    tick = 360 / 60;
+  console.log(hr, min, sec);
+
+  var deltaHr = (hr * 360) / 12 + (min * 360) / 60 / 12,
+    deltaMin = (min * 360) / 60 + (sec * 360) / 60 / 60,
+    deltaSec = (sec * 360) / 60;
+
+  var HOURHAND = document.querySelector("#hour"),
+    MINUTEHAND = document.querySelector("#minute"),
+    SECONDHAND = document.querySelector("#second");
+
+  HOURHAND.style.transform = "rotate(" + deltaHr + "deg)";
+  MINUTEHAND.style.transform = "rotate(" + deltaMin + "deg)";
+  SECONDHAND.style.transform = "rotate(" + deltaSec + "deg)";
+}
+
+var interval = setInterval(workingClock, 1000);
